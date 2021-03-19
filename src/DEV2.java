217ebfa96b49b42b53292096939e5b9bdb91009f
GrzZac <grzzac000@utp.edu.pl>
2021-03-19 20:22:53 +0100

    DrugiepoSpacji

---
+++ src/DEV2.java
@@ -0,0 +1,9 @@
+public class DEV2 {
+
+    public static void main(String [] args) {
+
+        System.out.println("Author: Grzegorz Zacharek");
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
 
+
+    public static void main(String [] args) {
+
+        System.out.println("jdk 8.0.2410.7");
+        System.out.println("jre 8u281");
+
+    }
     //jdk 8.0.2410.7
     //jre 8u281
 
